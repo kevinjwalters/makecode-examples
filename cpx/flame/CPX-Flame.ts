58cc9fc9d90bff505fd86756a2f511d5e1a53060
Kevin J Walters <kjw.github@gmail.com>
2018-10-09 12:16:33 +0100

    Implementing the burn variable in CPX-Flame.ts to represent the flame "temperature" with a new calcHS function to translate to hue and saturation.

--- cpx/flame/CPX-Flame.ts
+++ cpx/flame/CPX-Flame.ts
@@ -1,26 +1,38 @@
-// CPX-Flame6 (Revision 6)
+// CPX-Flame (Revision 7)
 // MIT License
 // Copyright (c) 2018 Kevin J. Walters
 //
-// https://makecode.com/_765F8JD9FTqc
+// https://makecode.com/_ejJfqW4MzWwt
 //
-let hue = 0
 let bright = 0
 let ledposy: number[] = []
-let huedelta = 0
 let distancesqrd = 0
 let posy = 0
 let ledposx: number[] = []
-let movey = 0
+let burndelta = 0
 let distance = 0
 let posx = 0
-let movex = 0
+let movey = 0
 let posdiffy = 0
-let steps = 0
+let hue = 0
+let burn = 0
+let movex = 0
 let posdiffx = 0
 let striplenminusone = 0
-let burn = 0
+let sat = 0
+let steps = 0
 let strip: light.NeoPixelStrip = null
+// Calculate the hue and saturation values based on
+// burn value.
+function calcHS() {
+    if (burn > 75) {
+        sat = 255 - (burn - 75) * 0.2
+        hue = 25
+    } else {
+        sat = 255
+        hue = burn / 75 * 25
+    }
+}
 function makeflame() {
     for (let index = 0; index <= striplenminusone; index++) {
         posdiffx = posx - ledposx[index]
@@ -36,7 +48,7 @@ function makeflame() {
             // the flame.
             bright = (2304 - distancesqrd) * 0.11
         }
-        strip.setPixelColor(index, light.hsv(hue, 255, bright))
+        strip.setPixelColor(index, light.hsv(hue, sat, bright))
     }
     strip.show()
 }
@@ -57,13 +69,14 @@ ledposx = [-8.5, -15, -17, -15, -8.5, 8.5, 15, 17, 15, 8.5]
 ledposy = [15, 8, 0, -8, -15, -15, -8, 0, 8, 15]
 posx = 0
 posy = 0
-// 25 is yellow
-hue = 25
+// A very approximate representation of candle
+// temperature in special units: 40 is red, 100 is
+// getting towards white.
+burn = 40
+sat = 255
 // Many of the values chosen here are based on a suck
 // it and  see (empirical) approach
 forever(function () {
-    // Not used yet, intended for flame brightness.
-    burn = 5
     steps = Math.randomRange(7, 12)
     // Calculate an amount to move per step for the x
     // position of flame based on a new random position.
@@ -72,14 +85,15 @@ forever(function () {
     // position of flame based on a new random position.
     movey = (Math.randomRange(-50, 50) - posy) / steps
     // Vary the hue (colour) a little
-    huedelta = (Math.randomRange(18, 28) - hue) / steps
+    burndelta = (Math.randomRange(40, 100) - burn) / steps
     for (let i = 0; i < steps; i++) {
         console.logValue("posx", posx)
         console.logValue("posy", posy)
+        calcHS()
         makeflame()
         posx += movex
         posy += movey
-        hue += huedelta
+        burn += burndelta
         pause(__internal.__timePicker(80))
     }
 })
